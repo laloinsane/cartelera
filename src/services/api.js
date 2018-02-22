21a887f38a0182998d447449e8016b264f184e16
laloinsane <calisto.michel@gmail.com>
2018-02-22 18:57:11 -0300

    Filtro por categorias fncional

--- src/services/api.js
+++ src/services/api.js
@@ -11,5 +11,6 @@ let getCategorias = () => {
 }
 
 export {
-    getActividades
+    getActividades,
+    getCategorias
 }
--- src/views/Categorias.vue
+++ src/views/Categorias.vue
@@ -1,11 +1,72 @@
 <template>
   <div>
-    <h1>Categorias</h1>
+    <section>
+      <label>Selecciona tu categoria:</label>
+      <select v-model="select">
+        <option v-for="categoria in categorias"
+          :key="categoria.id">
+          {{categoria.area}}
+        </option>
+      </select>
+    </section>
+    <section v-if="select !== ''">
+      <h1>Lista de todas las actividades</h1>
+      <ul>
+        <li v-for="actividad in filteredCategoria" 
+            :key="actividad.id"
+            @click.prevent="goToActividad(actividad)">
+            <actividad-card :actividad="actividad"></actividad-card>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
+import ActividadCard from '@/components/ActividadCard'
+import {getCategorias} from '@/services/api'
+import {getActividades} from '@/services/api'
+import _ from 'lodash'
 export default {
-  name: 'CategoriasView'
+  name: 'CategoriasView',
+  data() {
+    return {
+    categorias: [],
+    select: '',
+    actividades: []
+    }
+  },
+  created () {
+    getCategorias().then(response => {
+      //console.log(response.data)
+      this.categorias = response.data
+    }).catch(err => console.error(err))
+    getActividades().then(response => {
+      //console.log(response.data)
+      this.actividades = response.data
+    }).catch(err => console.error(err))
+  },
+  computed: {
+    filteredCategoria () {
+      let filteredCategoria = (this.select === '') ? this.actividades : this.actividades.filter(item => {
+        return _.includes(item.area, this.select)
+      })
+      return filteredCategoria.slice(0, this.limit)
+    }
+  },
+  methods: {
+    goToActividad (actividad) {
+      this.$router.push({
+        params: {
+          id: actividad.id,
+          actividad: actividad
+        },
+        name: 'Actividad'
+      })
+    }
+  },
+  components: {
+    ActividadCard
+  }
 }
 </script>
