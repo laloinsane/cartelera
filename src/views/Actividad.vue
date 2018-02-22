0b33edefe44d6a332c2a5f76924a14acee335268
laloinsane <calisto.michel@gmail.com>
2018-02-22 14:50:03 -0300

    Imput filtered actividad implementado

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -6,12 +6,23 @@ export default {
     if (!actividad) {
       this.$router.push({ name: 'Inicio' })
     } else {
-      console.log(actividad)
+      //console.log(actividad)
       return <section>
+        <h3>{ actividad.Creado_El }</h3>
         <h3>{ actividad.id }</h3>
         <h3>{ actividad.Fecha }</h3>
+        <h3>{ actividad.Fecha_ini }</h3>
+        <h3>{ actividad.Hora_Ini }</h3>
+        <h3>{ actividad.Hora_Fin }</h3>
         <h3>{ actividad.actividad }</h3>
+        <h3>{ actividad.Descripcion }</h3>
         <h3>{ actividad.direccion }</h3>
+        <h3>{ actividad.lugar }</h3>
+        <h3>{ actividad.En_Cartelera }</h3>
+        <h3>{ actividad.area }</h3>
+        <h3>{ actividad.color_area }</h3>
+        <h3>{ actividad.subarea }</h3>
+        <img src={ actividad.RutaImg }/>
       </section>
     }
   }
--- src/views/Actividades.vue
+++ src/views/Actividades.vue
@@ -1,31 +1,51 @@
 <template>
   <div>
-    <h1>Lista de todas las actividades</h1>
-    <ul>
-			<li v-for="actividad in actividades" 
-          :key="actividad.id" 
-          @click.prevent="goToActividad(actividad)">
-          <actividad-card :actividad="actividad"></actividad-card>
-  		</li>
-		</ul>
+    <section>
+      <label>Busca tu actividad:</label>
+      <input
+          type="text"
+          v-model="filter"
+          placeholder="Introduce el nombre de tu actividad">
+      <small>Actualmente solo se pueden hacer búsquedas por nombre</small>
+    </section>
+    <section>    
+      <h1>Lista de todas las actividades</h1>
+      <ul>
+        <li v-for="actividad in filteredActividad" 
+            :key="actividad.id" 
+            @click.prevent="goToActividad(actividad)">
+            <actividad-card :actividad="actividad"></actividad-card>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
 import ActividadCard from '@/components/ActividadCard'
 import {getActividades} from '@/services/api'
+import _ from 'lodash'
 export default {
   name: 'ActividadesView',
   data() {
     return {
-	  actividades: []
+    actividades: [],
+    filter: ''
     }
   },
   created () {
     getActividades().then(response => {
-      console.log(response.data)
+      //console.log(response.data)
       this.actividades = response.data
     }).catch(err => console.error(err))
+  },
+  computed: {
+    filteredActividad () {
+      let filteredActividad = (this.filter === '') ? this.actividades : this.actividades.filter(item => {
+        return _.includes(item.actividad.toLowerCase(), this.filter.toLowerCase())
+      })
+      return filteredActividad.slice(0, this.limit)
+    }
   },
   methods: {
     goToActividad (actividad) {
